ebdd98a5a73ce4e47544a021233bda963b057819
Mustafa TURAN <mustafaturan.net@gmail.com>
2016-08-28 00:09:40 +0100

    Added more tests.

--- test/bang_test.exs
+++ test/bang_test.exs
@@ -4,7 +4,8 @@ defmodule BangTest do
 
   test "create bang functions for module" do
     expected_func_list = [good_bye: 1, good_bye: 2, good_bye!: 1, titleize: 1,
-      welcome: 0, welcome: 1, welcome: 2, welcome!: 0, welcome!: 1, welcome!: 2]
+      welcome: 0, welcome: 1, welcome: 2, welcome: 3, welcome!: 0, welcome!: 1,
+      welcome!: 2, welcome!: 3]
     assert expected_func_list == SampleTestModule.__info__(:functions)
   end
 
@@ -15,5 +16,9 @@ defmodule BangTest do
     assert SampleTestModule.welcome!("turan") == result
     result = SampleTestModule.welcome("mr", "turan") |> SampleTestModule.titleize
     assert SampleTestModule.welcome!("mr", "turan") == result
+    result = SampleTestModule.welcome("mrs", "mr", "turan") |> SampleTestModule.titleize
+    assert SampleTestModule.welcome!("mrs", "mr", "turan") == result
+    result = SampleTestModule.good_bye("turan") |> SampleTestModule.titleize
+    assert SampleTestModule.good_bye!("turan") == result
   end
 end
--- test/support/sample_test_module.ex
+++ test/support/sample_test_module.ex
@@ -1,13 +1,15 @@
 defmodule SampleTestModule do
   use Bang
 
-  @bang {[welcome: 0, welcome: 1, welcome: 2], {SampleTestModule, :titleize}}
+  @bang {[welcome: 0, welcome: 1, welcome: 2, welcome: 3], {SampleTestModule, :titleize}}
 
   def welcome, do: "welcome"
   def welcome(name), do: "Welcome #{name}!"
   def welcome(prefix, name), do: "Welcome #{prefix}. #{name}!"
+  def welcome(prefix1, prefix2, name),
+    do: "Welcome #{prefix1} and #{prefix2} #{name}!"
 
-  @bang {[good_bye: 1, welcome: 2], {SampleTestModule, :titleize}}
+  @bang {[good_bye: 1], {SampleTestModule, :titleize}}
   def good_bye(name), do: "Good bye #{name}!"
   def good_bye(prefix, name), do: "Good bye #{prefix}. #{name}!"
 
